e33a9599cb9454d5730ef4d18a3822504af0898c
finn-01 <nguyensam576@gmail.com>
2022-01-09 22:28:19 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -4,13 +4,33 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import menu from "./data";
 
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
+	const [menuItems, setMenuItems] = useState(menu);
+	const [categories, setCategories] = useState(allCategories);
+
+	const filterItems = (category) => {
+		if (category === "all") {
+			setMenuItems(menu);
+			return;
+		}
+
+		const newItems = menu.filter((item) => item.category === category);
+		setMenuItems(newItems);
+	};
+
 	return (
-		<div>
-			Hello App
-			<Categories />
-			<Menu />
-		</div>
+		<main>
+			<section className="menu section">
+				<div className="title">
+					<h2>our menu</h2>
+					<div className="underline"></div>
+				</div>
+				<Categories categories={categories} filterItems={filterItems} />
+				<Menu menu={menuItems} />
+			</section>
+		</main>
 	);
 };
 
--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Menu = () => {
-	return <div>Component Menu</div>;
+const Menu = ({ menu }) => {
+	return (
+		<div className="section-center">
+			{menu.map((item) => {
+				const { id, title, img, desc, price } = item;
+				return (
+					<article className="menu-item" key={id}>
+						<img src={img} alt={title} className="photo" />
+						<div className="item-info">
+							<header>
+								<h4>{title}</h4>
+								<h4 className="price">{price}$</h4>
+							</header>
+							<p className="item-text">{desc}</p>
+						</div>
+					</article>
+				);
+			})}
+		</div>
+	);
 };
 
 export default Menu;
